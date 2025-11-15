afc17b2e42afb967e297795ba60cd246b771e930
Vini M <vinim@Vinis-MacBook-Pro.local>
2025-11-14 20:26:22 -0500

    Remove .next folder from repo

--- .next/server/app/api/weather/route.js
+++
@@ -1,7 +0,0 @@
-const CHUNK_PUBLIC_PATH = "server/app/api/weather/route.js";
-const runtime = require("../../../chunks/[turbopack]_runtime.js");
-runtime.loadChunk("server/chunks/node_modules_next_4b784988._.js");
-runtime.loadChunk("server/chunks/[root-of-the-server]__370af072._.js");
-runtime.getOrInstantiateRuntimeModule("[project]/.next-internal/server/app/api/weather/route/actions.js [app-rsc] (server actions loader, ecmascript)", CHUNK_PUBLIC_PATH);
-runtime.getOrInstantiateRuntimeModule("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/src/app/api/weather/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)", CHUNK_PUBLIC_PATH);
-module.exports = runtime.getOrInstantiateRuntimeModule("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/src/app/api/weather/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)", CHUNK_PUBLIC_PATH).exports;
--- .next/static/chunks/src_components_MapWidget_tsx_077928b2._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_8ff0e1f6._.js",
-  "static/chunks/src_components_MapWidget_tsx_eea8bab4._.js",
-  "static/chunks/node_modules_leaflet_dist_leaflet_88e19fd3.css"
-],
-    source: "dynamic"
-});
